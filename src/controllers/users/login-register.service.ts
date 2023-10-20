d36eecefae27bfd502186554c225abaa1440d523
23456-Tony-larrea <avs.larrea@yavirac.edu.ec>
2023-10-20 16:28:51 -0500

    fix register

--- src/controllers/users/login-register.service.ts
+++ src/controllers/users/login-register.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LoginRegisterService {}
+export class LoginRegisterService {
+    async register(data: { email: string, password: string, name: string, roleId: number }) {
+        return {
+            message: 'register'
+        };
+    }
+    
+}
